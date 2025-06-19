1d96d3869fd0a1167dd93dc8a8c24045e61943f9
freakyash11 <122809436+freakyash11@users.noreply.github.com>
2025-06-19 11:58:51 +0530

    updated package.json

--- services/firebase/client.ts
+++ services/firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+   apiKey: "AIzaSyC-ufLJypVHtJ_IWd0VdeK8qCHtKD956aQ",
+  authDomain: "prepwise-286c9.firebaseapp.com",
+  projectId: "prepwise-286c9",
+  storageBucket: "prepwise-286c9.firebasestorage.app",
+  messagingSenderId: "1096214464499",
+  appId: "1:1096214464499:web:b00d8c2d76b5c5e3b17486",
+  measurementId: "G-J0THYH0XEW",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
